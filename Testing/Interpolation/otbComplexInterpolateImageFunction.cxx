666c46abc6dc457b637810b1708b3e0cf711a502
Patrick Imbo <Patrick Imbo@localhost>
2011-08-17 16:07:01 +0200

    TST: writting a more generic test

--- Testing/Interpolation/otbComplexInterpolateImageFunction.cxx
+++ Testing/Interpolation/otbComplexInterpolateImageFunction.cxx
@@ -22,37 +22,17 @@
 #include "otbImageFileReader.h"
 #include <fstream>
 
-namespace Function
-{
-template<class TInput = double, class TOutput = double>
-class ComplexFunction
-{
-public:
-  void SetRadius(unsigned int rad)
-  {
-    m_Radius = rad;
-  }
-  unsigned int GetRadius() const
-  {
-    return m_Radius;
-  }
-  inline TOutput operator ()(const TInput& A) const
-  {
-    return static_cast<TOutput>(A);
-  }
-  unsigned int m_Radius;
-};
+#include "otbWindowedSincInterpolateImageBlackmanFunction.h"
 
-}
-
-int otbComplexInterpolateImageFunction(int argc, char * argv[])
+template<class TFunction>
+int otbComplexInterpolateImageFunction_generic(int argc, char * argv[])
 {
   const char * infname = argv[1];
   const char * outfname = argv[2];
 
   typedef std::complex<double>										InputPixelType;
   typedef otb::Image<InputPixelType, 2>								ImageType;
-  typedef Function::ComplexFunction<InputPixelType, InputPixelType>	FunctionType;
+  typedef TFunction												    FunctionType;
   typedef itk::ConstantBoundaryCondition<ImageType>					BoundaryConditionType;
   typedef double													CoordRepType;
 
@@ -63,10 +43,10 @@ int otbComplexInterpolateImageFunction(int argc, char * argv[])
   typedef InterpolatorType::ContinuousIndexType                     ContinuousIndexType;
   typedef otb::ImageFileReader<ImageType>                           ReaderType;
 
-  unsigned int radius = std::atoi(argv[3]);
-  double zeroFrequency = std::atof(argv[4]);
+  unsigned int radius = std::atoi(argv[4]);
+  double zeroFrequency = std::atof(argv[5]);
 
-  int i = 5;
+  int i = 6;
 
   std::vector<ContinuousIndexType> indicesList;
 
@@ -104,4 +84,19 @@ int otbComplexInterpolateImageFunction(int argc, char * argv[])
   file.close();
 
   return EXIT_SUCCESS;
+}
+
+
+int otbComplexInterpolateImageFunction(int argc, char * argv[])
+{
+  int FunctionType = atoi(argv[3]);
+  switch (FunctionType)
+    {
+    case 0:
+      return otbComplexInterpolateImageFunction_generic<otb::Function::BlackmanWindowFunction<double> > (argc, argv);
+      break;
+    default:
+      std::cerr << "No more function available\n";
+      return EXIT_FAILURE;
+    }
 }
