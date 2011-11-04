9f96dd1004aad80b98bbc76524e1d4068843152a
DuboisD <DuboisD@DuboisD-PC>
2011-11-04 17:06:29 -0400

    Updated method

--- Code/InterferogramFormation/otbFlatEarthRemovalFunctor.h
+++ Code/InterferogramFormation/otbFlatEarthRemovalFunctor.h
@@ -14,14 +14,16 @@ template<class TInput, class TOutput>
 class FlatEarthRemovalFunctor
 {
 public:
-  FlatEarthRemovalFunctor() : m_RangeFrequency(0.0) {};
+  FlatEarthRemovalFunctor() : m_RangeFrequency(0.0), m_AzimuthFrequency(0.0), m_Phase(0.0) {};
   ~FlatEarthRemovalFunctor() {}
 
   typedef itk::Index<2> IndexType;
 
   inline TOutput operator()( const TInput& inPix, IndexType index )
     {
-      TOutput phase(cos(otb::CONST_2PI*m_RangeFrequency*index[1]), sin(otb::CONST_2PI*m_RangeFrequency*index[1]));
+	//TOutput phase(vcl_cos(otb::CONST_2_PI*m_RangeFrequency*index[0]+otb::CONST_2_PI*m_AzimuthFrequency*index[1]+m_Phase), -(vcl_sin(otb::CONST_2_PI*m_RangeFrequency*index[0]+otb::CONST_2_PI*m_AzimuthFrequency*index[1]+m_Phase)));
+	//TOutput phase(vcl_cos(otb::CONST_2_PI*0.98*m_RangeFrequency*index[0]), -(vcl_sin(otb::CONST_2_PI*0.98*m_RangeFrequency*index[0]))); // 0.98 is a magic number
+	TOutput phase(vcl_cos(otb::CONST_2_PI*m_RangeFrequency*index[0]), -(vcl_sin(otb::CONST_2_PI*m_RangeFrequency*index[0])));
       return static_cast<TOutput>( inPix * phase );
     }
   void SetRangeFrequency(double value)
@@ -29,8 +31,20 @@ public:
     m_RangeFrequency = value;
   }
 
+  void SetAzimuthFrequency(double value)
+  {
+    m_AzimuthFrequency = value;
+  }
+
+  void SetPhaseAtMaxFreq(double value)
+  {
+	  m_Phase = value;
+  }
+
   private:
 	  double m_RangeFrequency;
+	  double m_AzimuthFrequency;
+	  double m_Phase;
 };
 
 } // end namespace functor
