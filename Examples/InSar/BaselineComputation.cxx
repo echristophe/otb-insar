01aee0d946330d8f571315dbab6ddedbc94ba468
Patrick Imbo <Patrick Imbo@localhost>
2012-02-04 09:56:11 +0100

    ENH: determine the slave plateform position

--- Examples/InSar/BaselineComputation.cxx
+++ Examples/InSar/BaselineComputation.cxx
@@ -27,13 +27,15 @@
 #include "otbImage.h"
 #include "otbImageFileReader.h"
 #include "otbPlatformPositionAdapter.h"
+#include "otbGenericRSTransform.h"
+
 
 int main(int argc, char* argv[])
 {
 
   if (argc != 3)
     {
-    std::cerr << "Usage: " << argv[0] << " masterImageFile slaveImageFile" << std::endl;
+    std::cerr << "Usage: " << argv[0] << " masterImageFile slaveImageFile demDir" << std::endl;
     return EXIT_FAILURE;
     }
 
@@ -50,6 +52,34 @@ int main(int argc, char* argv[])
 
   master->UpdateOutputInformation();
   slave->UpdateOutputInformation();
+  
+//  std::string demDir    = argv[3];
+
+  // Build wgs ref
+  OGRSpatialReference oSRS;
+  oSRS.SetWellKnownGeogCS("WGS84");
+  char * wgsRef = NULL;
+  oSRS.exportToWkt(&wgsRef);
+
+  typedef otb::GenericRSTransform<>         TransformType;
+
+  // Instantiate MasterImage->WGS transform
+  TransformType::Pointer masterImg2wgs = TransformType::New();
+  masterImg2wgs->SetInputProjectionRef(master->GetOutput()->GetProjectionRef());
+  masterImg2wgs->SetInputKeywordList(master->GetOutput()->GetImageKeywordlist());
+  masterImg2wgs->SetOutputProjectionRef(wgsRef);
+//  masterImg2wgs->SetDEMDirectory(demDir);
+  masterImg2wgs->InstanciateTransform();
+
+  // Instantiate WGS->Image transform
+  TransformType::Pointer wgs2SlaveImg = TransformType::New();
+  wgs2SlaveImg->SetInputProjectionRef(wgsRef);
+  wgs2SlaveImg->SetOutputProjectionRef(slave->GetOutput()->GetProjectionRef());
+  wgs2SlaveImg->SetOutputKeywordList(slave->GetOutput()->GetImageKeywordlist());
+//  wgs2SlaveImg->SetDEMDirectory(demDir);
+  wgs2SlaveImg->InstanciateTransform();
+
+
 
   typedef otb::PlatformPositionAdapter PlatformType;
   PlatformType::Pointer masterPlatform = PlatformType::New();
@@ -67,33 +97,45 @@ int main(int argc, char* argv[])
   std::vector<double> slaveSpeed;
   slavePosition.resize(3);
   slaveSpeed.resize(3);
+  
+  unsigned int numberOfLine  = master->GetOutput()->GetLargestPossibleRegion().GetSize()[0];
 
-  masterPlatform->GetPlatformPosition(0, masterPosition, masterSpeed);
-  slavePlatform->GetPlatformPosition(0, slavePosition, slaveSpeed);
 
-  std::cout << std::setprecision(15);
+  for(unsigned int i=0 ; i< numberOfLine ; i+=100)
+  {
+	  ImageType::PointType MasterImgPoint, estimatedMasterGeoPoint, estimatedSlaveImgPoint;
 
-  std::cout << "Master:\n";
-  std::cout << masterPosition[0] << "\n";
-  std::cout << masterPosition[1] << "\n";
-  std::cout << masterPosition[2] << "\n";
-  std::cout << masterSpeed[0] << "\n";
-  std::cout << masterSpeed[1] << "\n";
-  std::cout << masterSpeed[2] << "\n";
+    // References
+    MasterImgPoint[0] = i;
+    MasterImgPoint[1] = 0;
 
-  std::cout << "Slave:\n";
-  std::cout << slavePosition[0] << "\n";
-  std::cout << slavePosition[1] << "\n";
-  std::cout << slavePosition[2] << "\n";
-  std::cout << slaveSpeed[0] << "\n";
-  std::cout << slaveSpeed[1] << "\n";
-  std::cout << slaveSpeed[2] << "\n";
+	// Estimations Slave image point
+    estimatedMasterGeoPoint = masterImg2wgs->TransformPoint(MasterImgPoint);
+    estimatedSlaveImgPoint = wgs2SlaveImg->TransformPoint(estimatedMasterGeoPoint);
 
-  double baselineLength = vcl_sqrt(
-      (masterPosition[0] - slavePosition[0]) * (masterPosition[0] - slavePosition[0]) +
-      (masterPosition[1] - slavePosition[1]) * (masterPosition[1] - slavePosition[1]) +
-      (masterPosition[2] - slavePosition[2]) * (masterPosition[2] - slavePosition[2]));
-  std::cout << "Baseline length:\n";
-  std::cout << baselineLength << " m \n";
+
+	if(estimatedSlaveImgPoint[0] <0)
+	{
+		std::cout<<"MasterImgPoint #"<<i<<": ["<<MasterImgPoint[0]<<","<<MasterImgPoint[1]<<"]";
+		std::cout<<" -> estimatedSlaveImgPoint: "<<" ["<<estimatedSlaveImgPoint[0]<<","<<estimatedSlaveImgPoint[1]<<"]";
+		std::cout<<" -> No Baseline "<<std::endl;
+	}
+	else
+	{
+		masterPlatform->GetPlatformPosition(MasterImgPoint[0], masterPosition, masterSpeed);
+		slavePlatform->GetPlatformPosition(estimatedSlaveImgPoint[0], slavePosition, slaveSpeed);
+
+		std::cout << std::setprecision(15);
+
+	double baselineLength = vcl_sqrt(
+			(masterPosition[0] - slavePosition[0]) * (masterPosition[0] - slavePosition[0]) +
+			(masterPosition[1] - slavePosition[1]) * (masterPosition[1] - slavePosition[1]) +
+			(masterPosition[2] - slavePosition[2]) * (masterPosition[2] - slavePosition[2]));
+
+	std::cout<<"MasterImgPoint #"<<i<<": ["<<MasterImgPoint[0]<<","<<MasterImgPoint[1]<<"]";
+	std::cout<<" -> estimatedSlaveImgPoint: "<<" ["<<estimatedSlaveImgPoint[0]<<","<<estimatedSlaveImgPoint[1]<<"]";
+	std::cout<<" -> Baseline : " << baselineLength << " m "<<std::endl;
+	}
+  }
 
 }
