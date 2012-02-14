6bde2a0a7909921e8c91ce80eb55f88679fb7bd0
Patrick Imbo <Patrick Imbo@localhost>
2012-02-14 21:34:49 +0100

    ENH: add BaselineCalculusEnumType

---
+++ Code/Baseline/otbBaselineFunctorBase.cxx
@@ -0,0 +1,124 @@
+/*=========================================================================
+
+   Copyright 2011 Patrick IMBO
+   Contributed to ORFEO Toolbox under license Apache 2
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+=========================================================================*/
+#ifndef __otbBaselineFunctorBase_cxx
+#define __otbBaselineFunctorBase_cxx
+
+#include "otbBaselineFunctorBase.h"
+
+namespace otb
+{ 
+
+namespace Functor {
+
+
+double 
+BaselineFunctorBase
+::GetHorizontalBaseline() const
+{
+	itkExceptionMacro("GetHorizontalBaseline() not implemented");
+}
+
+double 
+BaselineFunctorBase
+::GetVerticalBaseline() const
+{
+	itkExceptionMacro("GetVerticalBaseline() not implemented");
+}
+
+double 
+BaselineFunctorBase
+::GetParallelBaseline() const
+{
+	itkExceptionMacro("GetParallelBaseline() not implemented");
+}
+
+double 
+BaselineFunctorBase
+::GetPerpendicularBaseline() const
+{
+	itkExceptionMacro("GetPerpendicularBaseline() not implemented");
+}
+
+double 
+BaselineFunctorBase
+::GetLengthBaseline() const
+{
+	itkExceptionMacro("GetLengthBaseline() not implemented");
+}
+
+double 
+BaselineFunctorBase
+::GetOrientationBaseline() const
+{
+	itkExceptionMacro("GetOrientationBaseline() not implemented");
+}
+
+double 
+BaselineFunctorBase
+::GetBaseline(BaselineCalculusEnumType map)
+{
+  switch( map )
+    {
+    case Horizontal:
+      {
+      return this->GetHorizontalBaseline();
+      break;
+      }
+    case Vertical:
+      {
+      return this->GetVerticalBaseline();
+      break;
+      }
+    case Parallel:
+      {
+      return this->GetParallelBaseline();
+      break;
+      }
+    case Perpendicular:
+      {
+      return this->GetPerpendicularBaseline();
+      break;
+      }
+    case Length:
+      {
+      return this->GetLengthBaseline();
+      break;
+      }
+    case Orientation:
+      {
+      return this->GetOrientationBaseline();
+      break;
+      }
+    }
+}
+
+void
+BaselineFunctorBase
+::PrintSelf( std::ostream& os, itk::Indent indent ) const
+{
+  Superclass::PrintSelf(os,indent);
+  os << " RTNBaseline vector : " << std::endl;
+  os << m_RTNBaseline << std::endl;
+}
+
+
+} // end namespace functor
+} // end namespace otb
+
+#endif
