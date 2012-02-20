97181741ab0fc53fafc5e661a205ed61a3c2cb0b
Patrick Imbo <Patrick Imbo@localhost>
2012-02-20 07:58:39 +0100

    TEST: enhance test for LengthOrientationFunctor class

--- Testing/Baseline/otbLengthOrientationBaselineFunctorTest.cxx
+++ Testing/Baseline/otbLengthOrientationBaselineFunctorTest.cxx
@@ -19,6 +19,7 @@
 #include "itkMacro.h"
 
 #include "otbLengthOrientationBaselineFunctor.h"
+#include "otbMath.h"
 
 
 int otbLengthOrientationBaselineFunctorTest(int argc, char * argv[])
@@ -31,9 +32,12 @@ int otbLengthOrientationBaselineFunctorTest(int argc, char * argv[])
   BaselineFunctorType::Pointer baseline = BaselineFunctorType::New();
   
   VectorType  RTNBaseline(3);
-  RTNBaseline[0] = 100.0;
-  RTNBaseline[1] = 200.0;
-  RTNBaseline[2] = 300.0;
+  double angle = 35.0; // in degree
+  double length = 100.0;
+  double degToRad = angle *otb::CONST_PI_180;
+  RTNBaseline[0] = -length * sin(degToRad);
+  RTNBaseline[1] = 0.0;
+  RTNBaseline[2] = length * cos(degToRad);
 
   baseline->SetRTNBaseline(RTNBaseline);
 
@@ -42,17 +46,19 @@ int otbLengthOrientationBaselineFunctorTest(int argc, char * argv[])
 									+ RTNBaseline[1]*RTNBaseline[1]
 									+ RTNBaseline[2]*RTNBaseline[2]);
 
-  if(std::abs(baseline->GetLengthBaseline() - resultLength) > 0.0 )
+  if(std::abs(baseline->GetLengthBaseline() - length) > 0.000001 )
   {
 	std::cout << "LengthBaseline : " << baseline->GetLengthBaseline() << std::endl;
-	std::cout << "Expected value : " << resultLength << std::endl;
+	std::cout << "Expected value : " << length << std::endl;
     return EXIT_FAILURE;
   }
 
-  if(std::abs(baseline->GetOrientationBaseline() -RTNBaseline[2]) >0.0)
+  double resultOrientation = std::atan2(-RTNBaseline[0],-RTNBaseline[2])* otb::CONST_180_PI;
+
+  if(std::abs(baseline->GetOrientationBaseline() -angle) > 0.0)
   {
 	std::cout << "OrientationBaseline : " << baseline->GetOrientationBaseline() << std::endl;
-	std::cout << "Expected value : " << RTNBaseline[2] << std::endl;
+	std::cout << "Expected value : " << angle << std::endl;
     return EXIT_FAILURE;
   }
 
